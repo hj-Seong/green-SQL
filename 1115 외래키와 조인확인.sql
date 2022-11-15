4b821edddee8df205afedd62b43f6a22bcaf281a
hj-Seong <hjseong1222@gmail.com>
2022-11-15 17:11:24 +0900

    1115 조인확인2

--- 1115 외래키와 조인확인.sql
+++ 1115 외래키와 조인확인.sql
@@ -27,3 +27,19 @@ insert into comment values (2, 1, "첫번째 게시글에 두번째 댓글");
 insert into comment values (3, 3, "세번째 게시글 첫번째 댓글");
 select *from comment;
 
+/* inner join을 통해서 내용 가져오기*/
+/* 테이블을 함께 가져올때, 필드의 이름도 테이블과 함께 작성해서 들고온다*/
+select post.postID, post.title, post.text, comment.text from post inner join comment
+	on post.postID = comment.postID;
+
+/* comment에 postID값 4를 추가하고 결과를 확인하세요 */
+insert into comment value (4, 4, "댓글추가");
+
+/* inner join을 사용하면 한쪽테이블에 값이 없을경우 양쪽다 출력되지않는다*/
+/* 한쪽 테이블을 전부 출력하고 싶을때 outter 사용할수 있다 (left/right)*/
+/* left join 왼쪽에 작성한(먼저 작성한) 테이블이 전부 출력되는 join*/
+select * from post left join comment
+	on post.postID = comment.postID;
+/* comment에서 post를 left join을 했을때 결과를 출력해보세요 */
+
+
